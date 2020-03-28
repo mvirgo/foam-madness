02ec6ea882bc380cab195f02678f3adeeebc7ac6
mvirgo <mvirgs44@gmail.com>
2020-03-27 18:23:34 -0700

    Initial Commit

---
+++ foam-madness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foam-madness
+//
+//  Created by Michael Virgo on 3/27/20.
+//  Copyright © 2020 mvirgo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
