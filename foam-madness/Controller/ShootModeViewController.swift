c84ae1eb630ff43a3c62e479d6a3532f974cf268
mvirgo <mvirgs44@gmail.com>
2020-03-27 21:32:41 -0700

    feat: clickable color-switching basketballs for shoot mode

--- foam-madness/Controller/ShootModeViewController.swift
+++ foam-madness/Controller/ShootModeViewController.swift
@@ -10,10 +10,32 @@ import UIKit
 
 class ShootModeViewController: UIViewController {
     
+    // MARK: IBOutlets
+    @IBOutlet weak var teamName: UILabel!
+    @IBOutlet weak var shotType: UILabel!
+    @IBOutlet weak var handSide: UILabel!
+    // Thanks https://stackoverflow.com/questions/50068413/array-of-uibuttons-in-swift-4
+    @IBOutlet var boxes : [UIButton]!
+    
+    // MARK: View functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set ball images based on click state
+        boxes.forEach {
+            $0.setBackgroundImage(UIImage(named: "basketball-gray"), for: .normal)
+            $0.setBackgroundImage(UIImage(named: "basketball"), for: .selected)
+        }
     }
     
+    // MARK: Other functions
+    
+    
+    // MARK: IBActions
+    @IBAction func ballButtonPressed(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+    }
+    @IBAction func finishedButtonPressed(_ sender: Any) {
+    }
     
 }
