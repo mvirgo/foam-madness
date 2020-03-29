43e9df941dd9584760680e1ef0bf63abe345ecb4
mvirgo <mvirgs44@gmail.com>
2020-03-29 12:27:02 -0700

    feat: appropriately pull stats and fg percentages from game data

--- foam-madness/Controller/GameStatsViewController.swift
+++ foam-madness/Controller/GameStatsViewController.swift
@@ -28,16 +28,41 @@ class GameStatsViewController: UIViewController {
     
     // MARK: Other functions
     func loadStats() {
-        // TODO: Get actual stats from datastore and replace below
-        var exTeam1 : [String] = ["KU", "Right", "100", "50%",
-                                  "55%", "35%", "60%", "80%"]
-        var exTeam2 : [String] = ["DU", "Left", "55", "30%",
-                                  "35%", "40%", "25%", "20%"]
+        // Get stats from teams and game
+        var exTeam1 : [String] = [team1.abbreviation!,
+                                  GameHelper.getHandSideString(game.team1Hand),
+                                  String(game.team1Score)]
+        exTeam1 += calcFGPercent([game.team1Ones, game.team1Twos,
+                                  game.team1Threes, game.team1Fours])
+        var exTeam2 : [String] = [team2.abbreviation!,
+                                  GameHelper.getHandSideString(game.team2Hand),
+                                  String(game.team2Score)]
+        exTeam2 += calcFGPercent([game.team2Ones, game.team2Twos,
+                                  game.team2Threes, game.team2Fours])
         
         for i in 0...team1Stats!.count-1 {
             team1Stats[i].text = exTeam1[i]
             team2Stats[i].text = exTeam2[i]
         }
+    }
+    
+    func calcFGPercent(_ shotCounts: [Int16]) -> [String] {
+        var out: [String] = []
+        let shots: Float = 10 // hard-coded number of shots per round
+        var totalMade: Int16 = 0 // counter for total FG%
+        
+        // Loop through shotCounts and calculate related FG%
+        for shotType in shotCounts {
+            totalMade += shotType
+            let percentage = Int((Float(shotType) / shots) * 100) // round to nearest int
+            out.append(String(percentage)+"%")
+        }
+        
+        // Add total FG% at start of out array
+        let totalPercentage = Int((Float(totalMade) / (shots * 4)) * 100)
+        out = [String(totalPercentage)+"%"] + out
+        
+        return out
     }
     
     // MARK: IBActions
