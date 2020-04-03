689d845815c1ab15113f1c9e52e9c9c42c2c31e3
mvirgo <mvirgs44@gmail.com>
2020-04-02 22:18:58 -0700

    feat: get user input for tournament name when creating

--- foam-madness/Controller/BracketCreationViewController.swift
+++ foam-madness/Controller/BracketCreationViewController.swift
@@ -13,11 +13,15 @@ class BracketCreationViewController: UIViewController {
     // MARK: IBOutlets
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var tourneyNameTextField: UITextField!
+    @IBOutlet weak var createTournamentButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
     
     // MARK: Other variables
     var dataController: DataController!
     var context: NSManagedObjectContext!
+    var tournament: Tournament!
+    var tournamentName: String!
     var bracketLocation: String!
     var regionSeedTeams = [String: [String: Int16]]()
     
@@ -30,20 +34,11 @@ class BracketCreationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Start motion on activity indicator
-        activityIndicator.startAnimating()
+        // Hide unneeded progress indictators
         activityIndicator.hidesWhenStopped = true
+        progressBar.isHidden = true
         // Get view context
         context = dataController.viewContext
-        // Load the bracket
-        loadBracket()
-        // Create any teams that aren't created yet
-        createAnyNewTeams()
-        // TODO: Create all games and add to tourney
-        // TODO: Ask user for name of the tournament
-        // TODO: Segue to table view of all playable games
-        // End activity indicator motion
-        activityIndicator.stopAnimating()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -109,5 +104,51 @@ class BracketCreationViewController: UIViewController {
         }
         // Add 10% to progress bar
         progressBar.progress += 0.10
+    }
+    
+    func alertUser(title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertVC, animated: true, completion: nil)
+    }
+    
+    func createTournamentObject() {
+        // Update the taskLabel
+        taskLabel.text = "Creating tournament object..."
+        // Create the tournament
+        tournament = Tournament(context: context)
+        tournament.name = tournamentName!
+        // Add 5% to progress bar
+        progressBar.progress += 0.05
+    }
+    
+    // MARK: IBActions
+    @IBAction func createTournamentButtonPressed(_ sender: Any) {
+        // Set the tournament name based on user input
+        if tourneyNameTextField.text != "" {
+            tournamentName = tourneyNameTextField.text
+        } else {
+            // Tell user to input a name
+            alertUser(title: "Invalid Name", message: "Tournament name cannot be blank.")
+            return
+        }
+        // Hide the text field and button
+        tourneyNameTextField.isHidden = true
+        createTournamentButton.isHidden = true
+        // Show the activity indicator and progress bar
+        activityIndicator.isHidden = false
+        progressBar.isHidden = false
+        // Start motion on activity indicator
+        activityIndicator.startAnimating()
+        // Load the bracket
+        loadBracket()
+        // Create any teams that aren't created yet
+        createAnyNewTeams()
+        // Create the tournament
+        createTournamentObject()
+        // TODO: Create all games and add to tourney
+        // End activity indicator motion
+        activityIndicator.stopAnimating()
+        // TODO: Segue to table view of all playable games
     }
 }
