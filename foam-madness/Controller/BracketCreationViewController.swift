172206d05b40484baf337bb522d2ad1f60460490
mvirgo <mvirgs44@gmail.com>
2020-04-02 20:16:29 -0700

    feat: load the selected bracket for tournament creation

--- foam-madness/Controller/BracketCreationViewController.swift
+++ foam-madness/Controller/BracketCreationViewController.swift
@@ -18,6 +18,7 @@ class BracketCreationViewController: UIViewController {
     // MARK: Other variables
     var dataController: DataController!
     var bracketLocation: String!
+    var regionSeedTeams = [String: [String: Int16]]()
     
     // MARK: View functions
     override func viewWillAppear(_ animated: Bool) {
@@ -26,9 +27,34 @@ class BracketCreationViewController: UIViewController {
         navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Start motion on activity indicator
+        activityIndicator.startAnimating()
+        // Load the bracket
+        loadBracket()
+        // TODO: Create any teams that aren't created yet
+        // TODO: Create all games and add to tourney
+        // TODO: Ask user for name of the tournament
+        // TODO: Segue to table view of all playable games
+        // End activity indicator motion
+        activityIndicator.stopAnimating()
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         // Re-show the navbar so it's there for other views
         navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    // MARK: Other functions
+    func loadBracket() {
+        // Update taskLabel for current task
+        taskLabel.text = "Loading bracket..."
+        // Load in bracket
+        let path = Bundle.main.path(forResource: bracketLocation, ofType: "plist")!
+        regionSeedTeams = NSDictionary(contentsOfFile: path)!.value(forKey: "Regions") as! Dictionary<String, [String: Int16]>
+        // Update progress bar to 5%
+        progressBar.progress = 0.05
     }
 }
