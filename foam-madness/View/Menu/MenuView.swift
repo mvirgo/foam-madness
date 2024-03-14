9307fd8946595b26bb4c490eff58a87b4070ed33
Michael Virgo <michael@sydecar.io>
2024-03-13 20:38:37 -0500

    feat: Improve menu view spacing on small screens

--- foam-madness/View/Menu/MenuView.swift
+++ foam-madness/View/Menu/MenuView.swift
@@ -13,51 +13,53 @@ struct MenuView: View {
     private var tournaments: FetchedResults<Tournament>
 
     var body: some View {
-        NavigationView {
-            VStack(spacing: 20) {
-                VStack(spacing: 10) {
-                    Text("Tournament Mode")
+        GeometryReader { geometry in
+            NavigationView {
+                VStack(spacing: geometry.size.height * 0.02) {
+                    VStack(spacing: 10) {
+                        Text("Tournament Mode")
+                        
+                        if (hasCurrentTournaments()) {
+                            NavigationLink(destination: SelectTournamentView(completedTournaments: false, tournaments: Array(tournaments))) {
+                                Text("Resume Tournament") }.buttonStyle(PrimaryButtonFullWidthStyle())
+                        }
+                        NavigationLink(destination: SelectInitialBracketView(isSimulated: false)) {
+                            Text("New Tournament") }.buttonStyle(PrimaryButtonFullWidthStyle())
+                        NavigationLink(destination: SelectInitialBracketView(isSimulated: true)) {
+                            Text("Quick Sim Tournament") }.buttonStyle(PrimaryButtonFullWidthStyle())
+                        if (hasCompletedTournaments()) {
+                            NavigationLink(destination: SelectTournamentView(completedTournaments: true, tournaments: Array(tournaments))) {
+                                Text("View Finished Tournament") }.buttonStyle(PrimaryButtonFullWidthStyle())
+                        }
+                    }.fixedSize(horizontal: true, vertical: false)
                     
-                    if (hasCurrentTournaments()) {
-                        NavigationLink(destination: SelectTournamentView(completedTournaments: false, tournaments: Array(tournaments))) {
-                            Text("Resume Tournament") }.buttonStyle(PrimaryButtonFullWidthStyle())
+                    VStack(spacing: geometry.size.height * 0.01) {
+                        Text("Single Game")
+                        NavigationLink(destination: SelectTeamsView()) {
+                            Text("Play Game")}.buttonStyle(PrimaryButtonStyle())
                     }
-                    NavigationLink(destination: SelectInitialBracketView(isSimulated: false)) {
-                        Text("New Tournament") }.buttonStyle(PrimaryButtonFullWidthStyle())
-                    NavigationLink(destination: SelectInitialBracketView(isSimulated: true)) {
-                        Text("Quick Sim Tournament") }.buttonStyle(PrimaryButtonFullWidthStyle())
-                    if (hasCompletedTournaments()) {
-                        NavigationLink(destination: SelectTournamentView(completedTournaments: true, tournaments: Array(tournaments))) {
-                            Text("View Finished Tournament") }.buttonStyle(PrimaryButtonFullWidthStyle())
+                    
+                    VStack(spacing: geometry.size.height * 0.01) {
+                        Text("Live Scores")
+                        NavigationLink(destination: LiveGamesView()) {
+                            Text("View Live Scores")}.buttonStyle(PrimaryButtonStyle())
                     }
-                }.fixedSize(horizontal: true, vertical: false)
-
-                VStack(spacing: 10) {
-                    Text("Single Game")
-                    NavigationLink(destination: SelectTeamsView()) {
-                        Text("Play Game")}.buttonStyle(PrimaryButtonStyle())
                 }
-
-                VStack(spacing: 10) {
-                    Text("Live Scores")
-                    NavigationLink(destination: LiveGamesView()) {
-                        Text("View Live Scores")}.buttonStyle(PrimaryButtonStyle())
+                .font(.system(size: 36))
+                .navigationTitle("Menu")
+                .navigationBarTitleDisplayMode(.inline)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        Text("Main Menu").font(.system(size: 24)).fontWeight(.bold)
+                    }
+                    ToolbarItem(placement: .topBarTrailing) { NavigationLink("About", destination: AboutView()).font(.system(size: 24))
+                    }
                 }
+                .minimumScaleFactor(0.8)
+                .padding([.bottom], 20)
             }
-            .font(.system(size: 36))
-            .navigationTitle("Menu")
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Text("Main Menu").font(.system(size: 24)).fontWeight(.bold)
-                }
-                ToolbarItem(placement: .topBarTrailing) { NavigationLink("About", destination: AboutView()).font(.system(size: 24))
-                }
-            }
-            .minimumScaleFactor(0.8)
-            .padding([.bottom], 40)
+            .navigationViewStyle(StackNavigationViewStyle())
         }
-        .navigationViewStyle(StackNavigationViewStyle())
     }
     
     private func hasCurrentTournaments() -> Bool {
